a77109fbc6637d5f15bf5fb27ef9194e38d3fbf3
dirtandgrass <verdant.luke@gmail.com>
2024-02-20 10:52:20 -0500

    add user friendly output

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ nextISSTimesForMyLocation((error, flyOverTimes) => {
     console.log(ERROR_FETCH, error);
     return;
   }
+  // loop over results/output
+  console.log("Upcoming ISS fly over times at your location:");
+  flyOverTimes.forEach((pass) => {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    // not sure why 2 spaces needed
+    console.log(`🛰️  ${datetime} for ⌚ ${duration} seconds!`);
+  });
 
-  console.log(flyOverTimes);
+
 });
